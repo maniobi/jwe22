49a453950940dc662a860b238469b74d2e299550
Manuel Obermoser <mo@obinet.at>
2022-01-18 21:31:50 +0100

    SkillCheck Teamwork Session

---
+++ javascript/script.js
@@ -0,0 +1,55 @@
+let deinName = 'Nik'; // string
+
+let alter = 26; // integer
+
+let meinAuto = {
+    marke: 'Audi',
+    ps: 150
+};
+
+let leute = [
+    'Paul',
+    'Samson',
+    'Dumbo'
+];
+
+let countries = [
+    "Polen", 
+    "Rumänien", 
+    "Norwegen",
+    "Österreich",
+    "Italien",
+    "Spanien",
+    "Schweiz"
+]; // array
+
+let regal = []; // undefined
+
+
+let i = 3;
+let x = 1;
+countries.splice(i, x) // Entfernt Elemente einer Anzahl x ab dem Index i
+// Österreich wird gelöscht
+
+
+
+let spruch = 'Ich bin nice, weil ich heiße ...';
+
+let zeichenkette = spruch + deinName;
+
+let zeichenkette2 = `Ich heiße ${deinName}, und ich lerne JavaScript.`;
+
+let zeichenkette3 = `${deinName} ist mein Name. Und ich bin ${alter} Jahre alt.`
+
+
+let zeichenkette4 = `Ich habe ein Auto mit der Marke ${meinAuto.marke}, und das hat ${meinAuto.ps} PS`;
+
+let zeichenkette5 = `${leute[2]} kenne ich aus einem Zeichentrickfilm. Das ist ein Elefant.`;
+
+
+let zahl1 = 1;
+let zahl2 = 4;
+
+if( zahl1 != zahl2 ) { // true
+    // lkjs dflkajsd ölfajskdf
+}
